929915e9c7765f33cdb109c505fa4f54bcf978c8
Meelis Utt <meelis.utt@gmail.com>
2023-10-31 20:23:23 +0100

    char to bin representation

--- example_handy.c
+++ example_handy.c
@@ -6,4 +6,9 @@ int main(int argc, char **argv) {
 	printf("program name='%s'\n",pname);
 	printf("program name length=%d\n", mystrlen(pname));
 	printf("program name is 'incorrect_name'=%d (0 is false, 1 is true)\n", mystrcomp("incorrect_name", pname));
+	char c = 'b';
+	printf("character '%c' (as int '%d') as binary representation: '%s'\n", c, c, ctob(c));
+	c = 'c';
+	printf("character '%c' (as int '%d') as binary representation: '%s'\n", c, c, ctob(c));
+
 }
