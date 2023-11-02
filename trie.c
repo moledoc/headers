1e67e5d2b977a964244e7181b6d0d49564fedabf
Meelis Utt <meelis.utt@gmail.com>
2023-11-02 22:08:50 +0100

    fix free error

--- trie.c
+++ trie.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TRIE_CHILDREN 1
-
 // A(0)BCDEFGHIJKLMNOPQRSTUVWXYZ(25)abcdefghijklmnopqrstuvwxyz(51)0123456789_-(63)
 #define TRIE_CHILDREN_SIZE 64
+#define TRIE_PRINT_PATH_SIZE 1024
 
 enum bool { false=0, true=1};
 
@@ -15,22 +14,22 @@ typedef struct trie_node {
 	struct trie_node *parent;
 } trie_node;
 
-// TODO: double free or corruption (out) error
 void trie_free(trie_node *tn) {
-	printf("HERE1 %s %p %p\n", tn->v, tn, tn->children);
-	for (int i=0; i<TRIE_CHILDREN_SIZE; ++i){
+	for (int i=0; !tn->is_leaf && i<TRIE_CHILDREN_SIZE; ++i){
 		if (tn->children[i] != NULL) {
 			trie_free(tn->children[i]);
-			free(tn->children[i]);
+			tn->children[i] = NULL;
 		}
 	}
-	printf("HERE2 %s %p %p\n", tn->v, tn, tn->children);
-	if (tn->children) {
+	if (*tn->children) {
 		free(tn->children);
+		tn->children = NULL;
 	}
 	if (tn->v) {
 		free(tn->v);
+		tn->v = NULL;
 	}
+	tn->parent = NULL;
 }
 
 void trie_print_rec(trie_node *tn, char ***path, int *path_size, int i) {
@@ -58,7 +57,6 @@ void trie_print_rec(trie_node *tn, char ***path, int *path_size, int i) {
 	}
 }
 
-#define TRIE_PRINT_PATH_SIZE 1024
 void trie_print(trie_node *tn) {
 	char **path = calloc(TRIE_PRINT_PATH_SIZE, sizeof(char *));
 	int path_size = TRIE_PRINT_PATH_SIZE;
@@ -80,7 +78,6 @@ trie_node *trie_new(trie_node *parent, const char *v, size_t vlen) {
 }
 
 int trie_add(trie_node *tn, const char *v) {
-	trie_node *tnc = tn;
 	char *vc = (char *)v;
 	size_t offset = 0;
 	while (*vc != '\0') {
@@ -97,18 +94,18 @@ int trie_add(trie_node *tn, const char *v) {
 		} else if (*vc == '-') {
 			idx = 63;
 		}
-		trie_node *child = tnc->children[idx];
+		trie_node *child = tn->children[idx];
 		if (child == NULL) {
-			child = trie_new(tnc, v, offset);
-			tnc->children[idx] = child;
+			child = trie_new(tn, v, offset);
+			tn->children[idx] = child;
 		}
-		if (tnc->is_leaf) {
-			tnc->is_leaf = false;
+		if (tn->is_leaf) {
+			tn->is_leaf = false;
 		}
-		tnc = child;
+		tn = child;
 		++vc;
 	}
-	tnc->is_leaf = 1;
+	tn->is_leaf = 1;
 	return 0;
 }
 
@@ -124,8 +121,9 @@ int trie_delete(trie_node *tn, char *v){
 
 int main(int argc, char **argv) {
 	trie_node *root = trie_new(NULL, "root", 4);
-	printf("%p %s %p %p\n", root, root->v, root->children, NULL);
 	trie_add(root, "Str");
+	trie_add(root, "Stra");
+	trie_add(root, "str");
 	trie_add(root, "String");
 	trie_add(root, "Street");
 	trie_print(root);
