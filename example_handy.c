2d0ffaf0ce87d018a308a91a87787e842f2fbb12
Meelis Utt <meelis.utt@gmail.com>
2023-11-18 16:15:42 +0100

    improve/walk (#4)
    * improve walk
    
    add free and print funcs

--- example_handy.c
+++ example_handy.c
@@ -22,19 +22,9 @@ int main(int argc, char **argv) {
 
 	// walk
 	char **files = calloc(256, sizeof(char *));
-	size_t file_count = 0;
 	char **dirs = calloc(256, sizeof(char *));
-	size_t dir_count = 0;
-	ftree ft = {files, 256, dirs, 256};
-	walk("/home/utt/c", &ft, &file_count, &dir_count);
-	for (int i=0; i<dir_count; ++i) {
-		printf("dir: %s\n", dirs[i]);
-		free(dirs[i]);
-	}
-	for (int i=0; i<file_count; ++i) {
-		printf("file: %s\n", files[i]);
-		free(files[i]);
-	}
-	free(files);
-	free(dirs);
+	ftree ft = {files, 256, 0, dirs, 256, 0};
+	walk(".", &ft);
+	ftree_print(ft);
+	ftree_free(ft);
 }
