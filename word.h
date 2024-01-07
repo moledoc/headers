c911461b5644663758fdf7fd7fa85746ad4d8fcc
Meelis Utt <meelis.utt@gmail.com>
2024-01-07 23:04:26 +0100

    add word.h template

---
+++ word.h
@@ -0,0 +1,16 @@
+// TODO: my string header
+
+#ifndef WORD_H_
+#define WORD_H_
+typedef struct {
+	char *s;
+	size_t len;
+} word;
+
+// TODO:
+
+#end // WORD_H_
+
+#ifdef WORD_IMPLEMENTATION
+// TODO:
+#end // WORD_IMPLEMENTATION
