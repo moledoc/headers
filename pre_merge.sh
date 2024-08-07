2e4183f544abd6a6e977db837083a6a707cbed1e
Meelis Utt <meelis.utt@gmail.com>
2024-08-08 00:00:18 +0200

    fix -DTWO_SUM
    valgrind reports memory leak

--- pre_merge.sh
+++ pre_merge.sh
@@ -2,5 +2,8 @@
 
 set -xe
 
-clang main.c -DSINGLY_LINKED_LIST -DMAP -DMD5 -DLOG -DUTILS -DSTACK -DQUEUE && ./a.out
+clang main.c -DSINGLY_LINKED_LIST -DSTACK -DQUEUE -DMAP -DTWO_SUM && ./a.out
+
+// target: clang main.c -DSINGLY_LINKED_LIST -DCIRCULAR_DOUBLY_LINKED_LIST -DMAP -DTWO_SUM -DMD5 -DLOG -DUTILS -DLEX -DSTACK -DQUEUE && ./a.out
+
 valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all ./a.out
