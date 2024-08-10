cba1661e6269f8044414f15d3706c4689fb66f39
Meelis Utt <meelis.utt@gmail.com>
2024-08-10 21:48:36 +0200

    exploring region-based memory management

---
+++ memreg.h
@@ -0,0 +1,163 @@
+#pragma once
+
+// {
+#ifdef MEMREG // HEADER
+
+#include <stdint.h>
+
+typedef struct MemReg {
+  struct MemReg *next;
+  uint32_t capacity;
+  uint32_t offset;
+  uintptr_t data[];
+} MemReg;
+
+enum {
+  MEMREG_CAPACITY_DEFAULT = 1 * 1024,
+};
+
+uint32_t MEMREG_CAPACITY = MEMREG_CAPACITY_DEFAULT;
+
+// memreg_zero zeros the region
+void memreg_zero(MemReg *region);
+
+// maps memory
+MemReg *memreg_create();
+
+// unmaps memory
+void *memreg_delete(MemReg *region);
+
+// memreg_alloc allocates memory
+void *memreg_alloc(MemReg *region, uint32_t data_size);
+
+void memreg_print(uint32_t data_size, char *fmt, ...);
+
+// memreg_dump dumps memory region metadata and data to stderr
+void memreg_dump(MemReg *region);
+
+#endif // MEMREG // HEADER
+
+// }
+
+// {
+#ifdef MEMREG // IMPLEMENTATION
+
+#include <stdarg.h>
+#include <sys/mman.h>
+#include <unistd.h>
+// #include <pthread.h> // TODO: add concurrency safety
+
+void memreg_zero(MemReg *region) {
+  for (int i = 0; i < region->capacity; ++i) {
+    region->data[i] = 0;
+  }
+}
+
+MemReg *memreg_create() {
+  int prot = PROT_READ | PROT_WRITE;
+  int flags = MAP_ANONYMOUS | MAP_PRIVATE;
+  int fd = -1;
+  off_t offset = 0;
+  size_t length = sizeof(MemReg) + sizeof(uintptr_t) * MEMREG_CAPACITY;
+  MemReg *region = mmap(NULL, length, prot, flags, fd, offset);
+  if (region == MAP_FAILED) {
+    // TODO: handle mmap failure
+  }
+  memreg_zero(region);
+  region->next = NULL;
+  region->capacity = MEMREG_CAPACITY;
+  region->offset = 0;
+  return region;
+}
+
+void *memreg_delete(MemReg *region) {
+  for (; region != NULL;) {
+    MemReg *me = region;
+    region = region->next;
+    int ret = munmap(me, sizeof(MemReg) + sizeof(uintptr_t) * me->capacity);
+    if (ret != 0) {
+      // TODO: handle failure
+    }
+  }
+  return NULL;
+}
+
+void *memreg_alloc(MemReg *region, uint32_t data_size) {
+  // check if fits in current region
+  // if yes, then return
+  // if no, then
+  // // if not exist, create
+  // check next region
+
+  data_size = (data_size + sizeof(uintptr_t) - 1) / sizeof(uintptr_t);
+
+  if (region->offset + data_size < region->capacity) {
+    uintptr_t *me = region->data + region->offset;
+    region->offset += 1; // data_size;
+    return me;
+  }
+
+  if (region->next == NULL) {
+    uint32_t bu = MEMREG_CAPACITY;
+    if (data_size > MEMREG_CAPACITY) {
+      MEMREG_CAPACITY = data_size;
+    }
+    MemReg *new = memreg_create();
+    region->next = new;
+    MEMREG_CAPACITY = bu;
+  }
+
+  return memreg_alloc(region->next, data_size);
+}
+
+void str_zero(char *dest, char val, size_t size) {
+  for (int i = 0; i < size; ++i) {
+    dest[i] = val;
+  }
+}
+
+void memreg_dump(MemReg *region) {
+  if (region == NULL) {
+    return;
+  }
+
+  FILE *stream = stdout;
+  fputs("----------------\n", stream);
+
+  char buf[65];
+  str_zero(buf, '\0', sizeof(buf));
+  snprintf(buf, sizeof(buf) - 1,
+           "next: %p\ncapacity: %u\noffset: %u\ndata: ", region->next,
+           region->capacity, region->offset);
+  write(fileno(stream), buf, sizeof(buf));
+
+  for (int i = 0; i < region->capacity; ++i) {
+    printf("%lu ", *(region->data + i));
+  }
+  putchar('\n');
+
+  return memreg_dump(region->next);
+}
+
+size_t str_len(const char *s1) {
+  size_t len = 0;
+  for (; (*s1++) != '\0';) {
+    ++len;
+  }
+  return len;
+}
+
+void memreg_print(uint32_t data_size, char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  char buf[str_len(fmt) + data_size + 1];
+  str_zero(buf, '\0', sizeof(buf));
+  vsnprintf(buf, sizeof(buf) - 1, fmt, args);
+  va_end(args);
+  write(fileno(stdout), buf, sizeof(buf));
+  return;
+}
+
+#endif // MEMREG // IMPLEMENTATION
+
+// }
---
+++ tmp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifndef MEMREG
+#define MEMREG
+#endif // MEMREG
+
+#include "memreg.h"
+
+typedef struct {
+  int k;
+  int v;
+} kv;
+
+char *kv_fmt = "HE3RE -- {%c:%d}\n";
+
+int main() {
+  MEMREG_CAPACITY = 10;
+
+  MemReg *region = memreg_create();
+
+  char *tst_str = "test";
+  char *tst = memreg_alloc(region, 1 * sizeof(char *));
+  memcpy(tst, tst_str, str_len(tst_str));
+
+  int *tst2 = memreg_alloc(region, 1 * sizeof(int *));
+  *tst2 = 'A';
+
+  kv *tst3 = memreg_alloc(region, 1 * sizeof(kv *));
+  tst3->k = 98;
+  tst3->v = 99;
+
+  memreg_print(4, "HE1RE -- %s\n", tst);
+  memreg_print(1, "HE2RE -- %s\n", tst2);
+  memreg_print(1 * sizeof(kv), kv_fmt, tst3->k, tst3->v);
+
+  memreg_dump(region);
+  region = memreg_delete(region);
+  memreg_dump(region);
+
+  return 0;
+}
